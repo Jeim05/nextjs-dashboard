b96e82aa6532fffab9e61c7bf6718009947bc655
Jeim05 <lopezvegajeimmy@gmail.com>
2024-03-21 22:35:58 -0600

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue, fetchLatestInvoices } from '@/app/lib/data';
+
+export default async function Dashboard() {
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+
+    </main>
+  )
+}
+
